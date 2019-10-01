2588bb983d4e75db87caa3fd73db32de8590ac33
dougalg <dougalg@gmail.com>
2019-10-01 11:12:42 +0700

    Fix sed command for unix

--- docs/trim-lines.sh
+++ docs/trim-lines.sh
@@ -5,6 +5,8 @@ declare -a arr=("PanHandler.js" "plugins.js" "plugins/ScrollZoom.js" "plugins/Cl
 ## now loop through the above array
 for i in "${arr[@]}"
 do
-	sed -i '' -e '/^exports\..*$/d' ./docs/out/assets/js/pan-handler/$i
-	sed -i '' -e '/^Object\.definePropert(exports,.*$/d' ./docs/out/assets/js/pan-handler/$i
+	echo Working on: $(pwd)/docs/out/assets/js/pan-handler/$i
+	# WARNING: This sed command fails on MacOS. You can install gsed to run it
+	sed -i -e '/^exports\..*$/d' "$(pwd)/docs/out/assets/js/pan-handler/$i"
+	sed -i -e '/^Object\.defineProperty(exports,.*$/d' "$(pwd)/docs/out/assets/js/pan-handler/$i"
 done
